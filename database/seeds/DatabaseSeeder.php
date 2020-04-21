324394612bb9a0a6b7c303b017ccc5a2f923e73e
jkinobe <jkinobe@gmail.com>
2020-04-21 12:18:42 +0300

    fixed config iseed error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+/*
         $this->call(UsersTableSeeder::class);
         $this->call(CustomFieldsTableSeeder::class);
         $this->call(CustomFieldValuesTableSeeder::class);
@@ -48,6 +49,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleHasPermissionsTableSeeder::class);
 
         $this->call(MediaTableSeeder::class);
-        $this->call(UploadsTableSeeder::class);
+        $this->call(UploadsTableSeeder::class);*/
     }
 }
